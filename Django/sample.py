bd033309e17eaaee3b415617075303ada3e522fd
Suriya <155054111+SURIYA-SURIYA@users.noreply.github.com>
2024-02-12 22:40:22 +0530

    Add files via upload

---
+++ Django/sample.py
@@ -0,0 +1,41 @@
+"""
+   Ptython Version check:
+   python --version
+
+
+   install pipenv package:  (virtual environment) 
+   pip install pipenv
+   
+   Ativate Virtual environment:
+   pipenv shell
+   
+   
+   check virtual environment:
+   pipenv --venv
+   
+   
+   terminl clear command:
+   cls
+   
+   Django insall:
+   pip install django
+   
+   check django-Admin Commands:
+   django-admin
+   
+   creat django project in same Directory;
+   Syntax :  django-admin startproject projectname .
+   Exampl :  django-admin startproject new_project .
+   
+   
+Run poject :
+ python manage.py runserver
+ 
+ server stop to:
+ contral + c
+ 
+ Additonal project port change to:
+  python manage.py runserver 8000
+ 
+   
+   """
